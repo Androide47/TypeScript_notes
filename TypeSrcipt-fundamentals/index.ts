ffe0e66d84e5072cc34cfd8f6bcce939af76b04a
Andrei Ortega <75287088+Androide47@users.noreply.github.com>
2024-03-18 16:25:03 -0600

    feat: updates

---
+++ TypeSrcipt-fundamentals/index.ts
@@ -0,0 +1,15 @@
+let sales = 123_456_789;
+let course = "TypeScript";
+let is_published = true;
+
+//This is a Any Type variable and it should be avoided as much as possible
+
+let level;
+level = 1;
+level = "a";
+
+let age = [];
+
+function render(document) {
+  console.log(document);
+}
